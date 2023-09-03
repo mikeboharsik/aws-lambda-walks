62ee41b53a85d20c449e8d510ee8f2855dfe760d
Mike Boharsik <mboharsik@gmail.com>
2023-09-02 22:49:57 -0400

    enhance distance and geojson calculations to account for common features composed of common features

--- client/src/util/geojson.js
+++ client/src/util/geojson.js
@@ -1,20 +1,29 @@
+function getGeoJsonFeaturesRecursively(allFeatures, feature) {
+	let features = [feature];
+	const commonFeatureIds = feature?.properties?.commonFeatureIds;
+	if (commonFeatureIds?.length) {
+		for (let i of commonFeatureIds) {
+			const f = allFeatures.find(e => e.properties.id === i);
+			features.push(...getGeoJsonFeaturesRecursively(allFeatures, f));
+		}
+	}
+	return features;
+}
+
 export function getGeojsonIoUrlForRoute(id, data) {
 	const targetRoute = data.find(route => route.properties.id === id);
 	if (!targetRoute) {
 		return;
 	}
 
+	const allFeatures = getGeoJsonFeaturesRecursively(data, targetRoute);
+
 	const updatedRouteData = {
 		type: "FeatureCollection",
-		features: [targetRoute],
+		features: allFeatures,
 	};
 
-	targetRoute.properties.commonFeatureIds?.forEach((commonFeatureId) => {
-		const feature = data.find(r => r.properties.id === commonFeatureId);
-		if (feature?.geometry) {
-			updatedRouteData.features.push(feature);
-		}
-	});
+	console.log(allFeatures);
 
 	const encodedRouteData = encodeURIComponent(JSON.stringify(updatedRouteData));
 	return `https://geojson.io/#data=data:application/json,${encodedRouteData}`;
@@ -22,4 +31,16 @@ export function getGeojsonIoUrlForRoute(id, data) {
 
 export function goToGeojsonIoUrlForRoute(id, data) {
 	window.open(getGeojsonIoUrlForRoute(id, data));
+}
+
+export function getRouteDistanceRecursively(allRoutes, route) {
+	let distance = route?.properties?.distance ?? 0;
+	const commonFeatureIds = route?.properties?.commonFeatureIds;
+	if (commonFeatureIds?.length) {
+		for (let featureId of commonFeatureIds) {
+			const targetFeature = allRoutes.find(e => e.properties.id === featureId);
+			distance += getRouteDistanceRecursively(allRoutes, targetFeature);
+		}
+	}
+	return distance;
 }
