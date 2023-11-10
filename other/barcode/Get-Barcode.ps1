44c67b89357ad4b18789ce567b7e4558ab3481b9
Mike Boharsik <mboharsik@gmail.com>
2023-11-10 14:59:45 -0500

    use mspaint alias

--- other/barcode/Get-Barcode.ps1
+++ other/barcode/Get-Barcode.ps1
@@ -3,7 +3,7 @@ Param(
 	[string] $Value
 )
 
-$PaintExePath = "C:\Program Files\WindowsApps\Microsoft.Paint_11.2309.28.0_x64__8wekyb3d8bbwe\PaintApp\mspaint.exe"
+$PaintExePath = "mspaint"
 
 node "$PSScriptRoot\index.js" $Value
 
