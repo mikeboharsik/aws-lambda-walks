2a2cb3feb4b1f1c6cbea84272b56c79e616587f5
Mike Boharsik <mboharsik@gmail.com>
2024-01-30 02:58:53 -0500

    add shuffleScheduledShorts.js

--- other/youtube/common.js
+++ other/youtube/common.js
@@ -27,7 +27,18 @@ function getAllVideoItems() {
 	return JSON.parse(fs.readFileSync('./uploads_videoitems.json'));
 }
 
+function shuffleArray(e) {
+	for (let i = e.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const temp = e[i];
+		e[i] = e[j];
+		e[j] = temp;
+	}
+	return e;
+}
+
 module.exports = {
 	getAllVideoItems,
 	getCustomArguments,
+	shuffleArray,
 };
