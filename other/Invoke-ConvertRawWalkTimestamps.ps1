118f16c266d4260a70a90fd4be189957b1b43ff5
Mike Boharsik <mboharsik@gmail.com>
2024-04-14 14:36:22 -0400

    add Invoke-ConvertRawWalkTimestamps tests

--- other/Invoke-ConvertRawWalkTimestamps.ps1
+++ other/Invoke-ConvertRawWalkTimestamps.ps1
@@ -1,12 +1,15 @@
 Param(
 	[Parameter(Mandatory=$true)]
 	[string] $Date,
-	[switch] $WhatIf,
+	[hashtable] $SourceJson,
 
-	[switch] $Test
+	[switch] $ReturnData,
+	[switch] $WhatIf
 )
 
 $ErrorActionPreference = "Stop"
+
+$zeroDuration = [TimeSpan]"00:00:00"
 
 function Copy-Array($array) {
 	$newArray = @()
@@ -20,47 +23,103 @@ function Copy-Array($array) {
 	return $newArray
 }
 
-$year, $month, $date = $Date -Split '-'
+function Get-Json {
+	$year, $month, $date = $Date -Split '-'
 
-if ($Test) {
-	$json = @{
-		start = "00:01:00.000"
-		end = "00:15:00.000"
-		exif = @(
-			@{ CreateDate = "2024:01:01 00:00:00"; Duration = "00:05:00" }
-			@{ CreateDate = "2024:01:01 00:00:00"; Duration = "00:05:00" }
-
-			@{ CreateDate = "2024:01:01 00:06:00"; Duration = "00:05:00" }
-			@{ CreateDate = "2024:01:01 00:06:00"; Duration = "00:05:00" }
-
-			@{ CreateDate = "2024:01:01 00:12:00"; Duration = "00:05:00" }
-			@{ CreateDate = "2024:01:01 00:12:00"; Duration = "00:05:00" }
-		)
-		events = @(
-			@{ mark = "00:00:30.000"; name = "First" }
-			@{ mark = "00:08:00.000"; name = "Second" }
-			@{ mark = "00:14:00.000"; name = "Third" }
-		)
-	}
-} else {
 	$pathToWalkRoutes = "$PSScriptRoot\..\..\walk-routes"
 	$filePath = Resolve-Path "$pathToWalkRoutes\meta_archive\$year\$month\$year-$month-$date.json"
 
-	$json = Get-Content $filePath
-		| ConvertFrom-Json -AsHashtable -NoEnumerate -Depth 10
+	if ($SourceJson) {
+		$json = $SourceJson
+	} else {
+		$json = Get-Content $filePath
+			| ConvertFrom-Json -AsHashtable -NoEnumerate -Depth 10
+	}
+
+	return $json
 }
 
-$segments = $json.exif | Select-Object -Unique CreateDate -ExpandProperty CreateDate | ForEach-Object { return [ordered]@{ createDate = $_; startDate = [DateTime]($_ -Replace '(\d{4}):(\d{2}):(\d{2})','$1-$2-$3') } }
-foreach ($segment in $segments) {
-	$parts = $json.exif | Where-Object { $_.CreateDate -eq $segment.createDate }
-	$duration = [TimeSpan]"00:00:00"
-	foreach ($part in $parts) {
-		$duration += [TimeSpan]$part.Duration
+function Get-SegmentsFromExif {
+	param($exif)
+
+	[hashtable[]]$segments = $exif
+		| Select-Object -Unique CreateDate -ExpandProperty CreateDate
+		| ForEach-Object {
+			return [hashtable]@{
+				createDate = $_
+				startDate = [DateTime]($_ -Replace '(\d{4}):(\d{2}):(\d{2})','$1-$2-$3')
+			}
+		}
+
+	foreach ($segment in $segments) {
+		$parts = $exif | Where-Object { $_.CreateDate -eq $segment.createDate }
+		$duration = $zeroDuration
+		foreach ($part in $parts) {
+			$duration += [TimeSpan]$part.Duration
+		}
+		$segment.duration = $duration
 	}
-	$segment.duration = $duration
+
+	return $segments
+}
+
+function Get-CalculatedSegmentData {
+	param($segments)
+
+	$jsonStart = [TimeSpan]$json.start
+
+	for ($i = 0; $i -lt $segments.Length; $i++) {
+		$cur = $segments[$i]
+		if ($i -eq 0) {
+			$cur.effectiveTrimmedStart = $zeroDuration
+			$cur.effectiveTrimmedEnd = $cur.duration - $jsonStart
+			$cur.totalGap = $zeroDuration
+		} else {
+			$last = $segments[$i - 1]
+
+			$cur.gapFromPrevious = $cur.startDate - ($last.startDate + $last.duration)
+	
+			$totalGap = $zeroDuration
+			for ($j = 1; $j -le $i; $j++) {
+				$t = $segments[$j]
+				$totalGap += $t.gapFromPrevious
+			}
+			$cur.totalGap = $totalGap
+	
+			$totalDuration = $zeroDuration
+			$totalDurationWithoutGaps = $zeroDuration
+			for ($j = 0; $j -le $i; $j++) {
+				$t = $segments[$j]
+				$totalDuration += [TimeSpan]$t.duration
+				$totalDurationWithoutGaps += [TimeSpan]$t.duration
+	
+				if ($t.gapFromPrevious) {
+					$totalDuration -= $t.gapFromPrevious
+				}
+			}
+			$cur.totalDuration = $totalDuration - $jsonStart
+			$cur.totalDurationWithoutGaps = $totalDurationWithoutGaps - $jsonStart
+	
+			$cur.effectiveTrimmedStart = $last.effectiveTrimmedEnd + $cur.gapFromPrevious
+			$cur.effectiveTrimmedEnd = $cur.effectiveTrimmedStart + $cur.duration - $cur.gapFromPrevious
+		}
+	}
+
+	return $segments
+}
+
+function Get-Segments {
+	param($json)
+
+	$segments = Get-SegmentsFromExif $json.exif
+	$segments = Get-CalculatedSegmentData $segments
+
+	return $segments
 }
 
 function Print-Nice {
+	param($segments)
+	
 	$copy = Copy-Array $segments
 
 	foreach ($segment in $copy) {
@@ -73,75 +132,60 @@ function Print-Nice {
 	Write-Host ($copy | ConvertTo-Json -Depth 10)
 }
 
-for ($i = 0; $i -lt $segments.Length; $i++) {
-	$cur = $segments[$i]
-	if ($i -gt 0) {
-		$last = $segments[$i - 1]
+function Print-Segments {
+	param($segments)
 
-		$cur.gapFromPrevious = ($cur.startDate - ($last.startDate + $last.duration)).ToString()
-
-		$totalGap = [TimeSpan]"00:00:00"
-		for ($j = 1; $j -le $i; $j++) {
-			$t = $segments[$j]
-			$totalGap += $t.gapFromPrevious
-		}
-		$cur.totalGap = $totalGap.ToString()
-
-		$totalDuration = [TimeSpan]"00:00:00"
-		$totalDurationWithoutGaps = [TimeSpan]"00:00:00"
-		for ($j = 0; $j -le $i; $j++) {
-			$t = $segments[$j]
-			$totalDuration += [TimeSpan]$t.duration
-			$totalDurationWithoutGaps += [TimeSpan]$t.duration
-
-			if ($t.gapFromPrevious) {
-				$totalDuration += $t.gapFromPrevious
+	Write-Host ($segments | ForEach-Object {
+		$n = @{}
+		foreach ($key in $_.Keys) {
+			if ($_[$key].GetType() -eq [TimeSpan]) {
+				$n[$key] = $_[$key].ToString()
+			} else {
+				$n[$key] = $_[$key]
 			}
 		}
-		$cur.totalDuration = $totalDuration.ToString()
-		$cur.totalDurationWithoutGaps = $totalDurationWithoutGaps.ToString()
-
-		$cur.effectiveTrimmedStart = $last.effectiveTrimmedEnd + $cur.gapFromPrevious
-		$cur.effectiveTrimmedEnd = $cur.effectiveTrimmedStart + $cur.duration
-	} else {
-		$cur.effectiveTrimmedStart = [TimeSpan]"00:00:00"
-		$cur.effectiveTrimmedEnd = $cur.duration - [TimeSpan]$json.start
-	}
+		return $n
+	} | ConvertTo-Json)
 }
+
+$json = Get-json
+$jsonStart = [TimeSpan]$json.start
+$segments = Get-Segments $json
+# Print-Segments $segments
 
 foreach ($event in $json.events) {
 	if (!$event.mark) { continue }
 
-	$adjustedStart = [TimeSpan]$event.mark - [TimeSpan]$json.start
+	if ($event.trimmedStart -and $event.trimmedEnd -and $event.trimmedStart -ne $event.trimmedEnd) {
+		Write-Host "Skipping event that has already been adjusted"
+		continue
+	}
 
-	$event['trimmedStart'] = $adjustedStart.ToString()
+	$mark = [TimeSpan]$event.mark
+
+	$targetSegment = $segments | Where-Object { $mark -ge $_.effectiveTrimmedStart -and $mark -lt $_.effectiveTrimmedEnd }
+	if (!$targetSegment) {
+		Write-Error "Failed to find segment for event"
+		exit 1
+	}
+
+	$adjustedStart = $mark - $jsonStart - $targetSegment.totalGap
+	$adjustedStart = $adjustedStart.ToString() -Replace '([\d]{3})\d+','$1'
+
+	$event['trimmedStart'] = $adjustedStart
 
 	if ($event.name -ne $null) {
-		$event['trimmedEnd'] = $adjustedStart.ToString()
+		$event['trimmedEnd'] = $adjustedStart
 	}
 }
 
 if ($WhatIf) {
+	$json.Remove("exif")
 	return $json | ConvertTo-Json -Depth 10
 }
 
-if ($Test) {
-	$asserts = @(
-		@{ name = 'Segments length is 3'; value = $segments.Length; expected = 3; }
-
-		@{ name = 'First event trimmedStart is -00:00:30'; value = $json.events[0].trimmedStart; expected = "-00:00:30" }
-		@{ name = 'Second event trimmedStart is 00:08:00'; value = $json.events[1].trimmedStart; expected = "00:08:00" }
-		# @{ name = 'Third event trimmedStart is -00:00:30'; value = $json.events[0].trimmedStart; expected = "-00:00:30" }
-	)
-
-	foreach ($assert in $asserts) {
-		$good = $assert.value -eq $assert.expected
-		if (!$good) {
-			throw "Assertion failed: $($assert.name) -> Value is [$($assert.value)]"
-		}
-	}
+if ($ReturnData) {
+	return $json
 } else {
-	$json
-		| ConvertTo-Json -Depth 10
-		| Set-Content $filePath
+	$json | ConvertTo-Json -Depth 10 | Set-Content $filePath $serialized
 }
