bf1fb727618b970ebd9096f224fe1e3c3d6939af
Mike Boharsik <mboharsik@gmail.com>
2024-06-18 10:47:34 -0400

    attempt to fix auth cache expiration

--- server/index.js
+++ server/index.js
@@ -129,7 +129,7 @@ exports.handler = async (event) => {
 			console.warn('Body seems to exist but it failed to parse', e);
 		}
 
-		if (event?.headers?.authorization) {
+		if (event?.headers?.authentication) {
 			await authenticate(event);
 		}
 
