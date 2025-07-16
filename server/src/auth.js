f5af3fd25cd739fd4820eae495e5a0de0605ce3a
Mike Boharsik <mboharsik@gmail.com>
2025-07-16 12:30:29 -0400

    refactor: authenticate function

--- server/src/auth.js
+++ server/src/auth.js
@@ -2,41 +2,47 @@ const jwt = require('jsonwebtoken');
 
 const authUrl = process.env.AUTH_URL;
 
-async function authenticate(event) {
-  let token;
+function verifyToken(event) {
+	const publicKey = Buffer.from(process.env.AUTH_PUBLIC_KEY, 'base64').toString();
+	const [, token = null] = event.headers?.authorization?.split('Bearer ') ?? [];
+
+	let verified = false;
 	try {
-		const publicKey = Buffer.from(process.env.AUTH_PUBLIC_KEY, 'base64').toString();
-		const [, token = null] = event.headers?.authorization?.split('Bearer ') ?? [];
-
-		let verified = false;
-		try {
-			if (token) {
-				verified = jwt.verify(token, publicKey, { algorithm: 'RS256' });
-			} else {
-				const { queryStringParameters: { jwt: queryJwt } = {} } = event;
-				if (queryJwt) {
-					verified = jwt.verify(queryJwt, publicKey, { algorithm: 'RS256' });
-				}
+		if (token) {
+			verified = jwt.verify(token, publicKey, { algorithm: 'RS256' });
+		} else {
+			const { queryStringParameters: { jwt: queryJwt } = {} } = event;
+			if (queryJwt) {
+				verified = jwt.verify(queryJwt, publicKey, { algorithm: 'RS256' });
 			}
-			console.log('Authenticated user:', verified?.sub);
-		} catch (e) {
-			console.error('Failed to verify JWT', e);
 		}
+		console.log('Authenticated user:', verified?.sub);
+	} catch (e) {
+		console.error('Failed to verify JWT', e, token);
+	}
+	return verified;
+}
 
-		if (!verified.scope.includes('walks.read')) {
-			console.log('Expected token scope to include walks.read but was', verified.scope);
-			verified = false;
-		}
+function verifyScope(token) {
+	const result = token.scope.includes('walks.read');
+	if (!result) console.log('Expected token scope to include walks.read but was', token.scope);
+	return result;
+}
 
+async function authenticate(event) {
+	try {
+		let verified = verifyToken(event);
+		verified &&= verifyScope(verified);
 		if (verified) {
 			event.isAuthed = true;
 			event.authSubject = verified.sub;
 			event.authExpires = new Date(verified.exp * 1000).toUTCString();
+			event.authScope = verified.scope;
 		} else {
       console.log('Authentication failed', authUrl, event.headers.authorization);
     }
 	} catch (e) {
-    console.error('Something went wrong during authentication', e, token);
+    console.error('Something went wrong during authentication', e);
   }
 }
 
