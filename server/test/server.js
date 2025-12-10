4fc3c6ec1ff06f6fa9a5809a8e5cd370967bb1c2
Mike Boharsik <mboharsik@gmail.com>
2025-12-10 10:36:12 -0500

    feat: support toggling auth

--- server/test/server.js
+++ server/test/server.js
@@ -5,9 +5,12 @@ const port = 2312;
 
 process.env.ENABLE_LOGGING = true;
 
+const disableAuth = process.argv[2] === 'true';
+console.log({ disableAuth });
+
 const server = http.createServer(async (req, res) => {
 	const [path, query] = req.url.split('?');
-	const result = await callHandler(path, query, req.headers, true);
+	const result = await callHandler(path, query, req.headers, !disableAuth);
 	res.writeHead(result.statusCode, { 'Access-Control-Allow-Origin': '*', ...result.headers });
 	res.write(result.body || '');
 	res.end();
