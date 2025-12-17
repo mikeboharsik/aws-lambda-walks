54f5411c58bf550b611bf0db7efc6d3c46f1c25b
Mike Boharsik <mboharsik@gmail.com>
2025-12-17 18:58:30 -0500

    feat: split artifact generation from deployment

---
+++ server/Invoke-CreateArtifact.ps1
@@ -0,0 +1,22 @@
+Param(
+	[string] $EventsPath = "../walk-routes/events.json",
+	[string] $GeneratedPath = "../walk-routes/generated/**"
+)
+
+Write-Host "Using arguments:`n"
+	` "    `$EventsPath = [$EventsPath]"
+	` "    `Get-Location = [$(Get-Location)]"
+
+$ErrorActionPreference = 'Stop'
+
+New-Item -Path "$PSScriptRoot/build" -ItemType Directory -Force | Out-Null
+
+Copy-Item -Path @("$PSScriptRoot/src/**", "$PSScriptRoot/node_modules") -Recurse -Destination "$PSScriptRoot/build" -Force | Out-Null
+
+Copy-Item -Path "$PSScriptRoot/../client/public" -Recurse -Destination "$PSScriptRoot/build/public" -Force | Out-Null
+
+Copy-Item -Recurse $GeneratedPath "$PSScriptRoot/build"
+
+Write-Host "Everything except node_modules:" (Get-ChildItem "$PSScriptRoot/build/**" -Recurse -File | Where-Object { $_.FullName -NotMatch "node_modules" } | ForEach-Object { $_.FullName } | ConvertTo-Json -Depth 10 -Compress)
+
+Compress-Archive -Path "$PSScriptRoot/build/**" -DestinationPath "$PSScriptRoot/build/deployable.zip" -Force
--- server/Invoke-Upload.ps1
+++ server/Invoke-Upload.ps1
@@ -1,7 +1,5 @@
 Param(
 	[string] $DistributionId,
-	[string] $EventsPath = "../walk-routes/events.json",
-	[string] $GeneratedPath = "../walk-routes/generated/**",
 
 	[string[]] $InvalidationPaths = @(
 		"/build/*"
@@ -15,18 +13,12 @@ Param(
 		"/api/youtubeIds"
 		"/global.css"
 		"/index.html"
-	),
-
-	[switch] $DeployClient,
-	[switch] $SkipUpload
+	)
 )
 
 Write-Host "Using arguments:`n" 
 	` "    `$DistributionId = [$DistributionId]"
-	` "    `$EventsPath = [$EventsPath]"
 	` "    `$InvalidationPaths = [$InvalidationPaths]"
-	` "    `$DeployClient = [$DeployClient]"
-	` "    `$SkipUpload = [$SkipUpload]"
 	` "    `Get-Location = [$(Get-Location)]"
 
 $ErrorActionPreference = 'Stop'
@@ -35,34 +27,18 @@ if (!$DistributionId) {
 	$DistributionId = Read-Host "No CloudFront distribution ID provided, please enter one or hit Enter to skip cache invalidation"
 }
 
-New-Item -Path "$PSScriptRoot/build" -ItemType Directory -Force | Out-Null
+aws --no-cli-pager lambda update-function-code --function-name "walks" --zip-file "fileb://$PSScriptRoot/build/deployable.zip" | ConvertFrom-Json -AsHashtable -Depth 10 | ConvertTo-Json -Depth 10 -Compress
 
-Copy-Item -Path @("$PSScriptRoot/src/**", "$PSScriptRoot/node_modules") -Recurse -Destination "$PSScriptRoot/build" -Force | Out-Null
+if ($DistributionId) {
+	$result = aws cloudfront create-invalidation --distribution-id $DistributionId --paths $InvalidationPaths | ConvertFrom-Json -AsHashtable
+	$invalidationId = $result.Invalidation.Id
+	Write-Host ($result | ConvertTo-Json -Depth 10 -Compress)
 
-if ($DeployClient) {
-	Copy-Item -Path "$PSScriptRoot/../client/public" -Recurse -Destination "$PSScriptRoot/build/public" -Force | Out-Null
-}
+	while ($result.Invalidation.Status -ne 'Completed') {
+		Start-Sleep -Seconds 5
 
-Copy-Item -Recurse $GeneratedPath "$PSScriptRoot/build"
-
-Write-Host "Everything except node_modules:" (Get-ChildItem "$PSScriptRoot/build/**" -Recurse -File | Where-Object { $_.FullName -NotMatch "node_modules" } | ForEach-Object { $_.FullName } | ConvertTo-Json -Depth 10 -Compress)
-
-Compress-Archive -Path "$PSScriptRoot/build/**" -DestinationPath "$PSScriptRoot/build/deployable.zip" -Force
-
-if (!$SkipUpload) {
-	aws --no-cli-pager lambda update-function-code --function-name "walks" --zip-file "fileb://$PSScriptRoot/build/deployable.zip" | ConvertFrom-Json -AsHashtable -Depth 10 | ConvertTo-Json -Depth 10 -Compress
-
-	if ($DistributionId) {
-		$result = aws cloudfront create-invalidation --distribution-id $DistributionId --paths $InvalidationPaths | ConvertFrom-Json -AsHashtable
-		$invalidationId = $result.Invalidation.Id
-		Write-Host ($result | ConvertTo-Json -Depth 10 -Compress)
-
-		while ($result.Invalidation.Status -ne 'Completed') {
-			Start-Sleep -Seconds 5
-
-			$result = aws cloudfront get-invalidation --distribution-id $DistributionId --id $invalidationId | ConvertFrom-Json -AsHashtable
-		}
-
-		Write-Host ($result | ConvertTo-Json -Depth 10 -Compress)
+		$result = aws cloudfront get-invalidation --distribution-id $DistributionId --id $invalidationId | ConvertFrom-Json -AsHashtable
 	}
+
+	Write-Host ($result | ConvertTo-Json -Depth 10 -Compress)
 }
