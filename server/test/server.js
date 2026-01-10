fc27bdcab7222144c9025f2222a60a0a88f0c741
Mike Boharsik <mboharsik@gmail.com>
2026-01-10 11:15:49 -0500

    feat: disable CORS header for now

--- server/test/server.js
+++ server/test/server.js
@@ -13,7 +13,7 @@ console.log({ ignoreAuth });
 const server = http.createServer(async (req, res) => {
 	const [path, query] = req.url.split('?');
 	const result = await callHandler(path, query, req.headers, ignoreAuth);
-	res.writeHead(result.statusCode, { 'Access-Control-Allow-Origin': '*', ...result.headers });
+	res.writeHead(result.statusCode, result.headers);
 	res.write(result.body || '');
 	res.end();
 });
