d527789de9590d031115e4bd37f2022b884e46cc
Mike Boharsik <mboharsik@gmail.com>
2026-01-29 14:06:07 -0500

    feat: remove event.startMs after we're done with it

--- server/src/util/logResult.js
+++ server/src/util/logResult.js
@@ -1,5 +1,6 @@
 function logResult(result, event) {
 	const totalMs = (performance.now() - event.startMs).toFixed(3);
+	delete event.startMs;
 	if (process.env.LOG_RESULT === 'true') {
 		event.log(`Returning result after ${totalMs}`, JSON.stringify(result, null, '  '));
 	} else {
