0fd1b50738e9115c5e062d1cf8f980b5b0a0db2c
Mike Boharsik <mboharsik@gmail.com>
2026-02-09 16:21:33 -0500

    feat: include currentTimeToTimestamp

--- server/src/handlers/EventsHandler.js
+++ server/src/handlers/EventsHandler.js
@@ -186,8 +186,15 @@ class EventsHandler extends ApiRequestHandler {
 					hits.forEach(event => {
 						Object.keys(event).forEach(key => {
 							if (key.toLowerCase() === target) {
+								function currentTimeToTimestamp(currentTime) {
+									const hours = Math.floor(currentTime / (60 * 60)).toString().padStart(2, 0);
+									const minutes = Math.floor((currentTime / 60) % 60).toString().padStart(2, 0);
+									const seconds = Math.floor(currentTime % 60).toString().padStart(2, 0);
+									const ms = (currentTime % 1).toFixed(3).padEnd(3, 0).replace('0.', '');
+									return `${hours}:${minutes}:${seconds}.${ms}`;
+								}
 								const seconds = event[key] / 1000;
-								event[key] = seconds;
+								event[key] = currentTimeToTimestamp(seconds);
 							}
 						});
 					});
